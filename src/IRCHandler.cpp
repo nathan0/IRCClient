b00d81ddc9c05ff7a60c53ec55f28644dfa7a610
nathan0 <nathan0@users.noreply.github.com>
2014-05-05 13:57:31 +0100

    Update IRCHandler.cpp

--- src/IRCHandler.cpp
+++ src/IRCHandler.cpp
@@ -61,7 +61,7 @@ void IRCClient::HandleCTCP(IRCMessage message)
     {
         if (text == "VERSION") // Respond to CTCP VERSION
         {
-            SendIRC("NOTICE " + message.prefix.nick + " :\001VERSION Open source IRC client by Fredi Machado - https://github.com/Fredi/IRCClient \001");
+            SendIRC("NOTICE " + message.prefix.nick + " :\001VERSION https://github.com/nathan0/IRCClient \001");
             return;
         }
 
